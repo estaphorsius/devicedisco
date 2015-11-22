8fb6ab3502846d55825a5e8773bb080286246966
estaphorsius <elmar@bether.net>
2015-11-22 20:18:20 +0100

    evening commit...

--- src/DeviceDiscovery.Test/DiscoveryListenerTests.cs
+++ src/DeviceDiscovery.Test/DiscoveryListenerTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
 using FakeItEasy;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -12,19 +15,69 @@ namespace DeviceDiscovery.Test
         [Test]
         public void T()
         {
-            var fakeSocket = A.Fake<ISocket>();
+            var fakeSocket = new MockedSocket();
             var fakeSocketFactory = A.Fake<ISocketFactory>();
             var fakeResponsFactory = A.Fake<IResponseFactory>();
-            var fakeMessageParser = A.Fake<IMessageParser>();
+            var messageParser = new MessageParser();
             var fakeDeviceInfoCollector = A.Fake<IDeviceInfoCollector>();
 
             A.CallTo(() => fakeSocketFactory.CreateListeningSocket()).Returns(fakeSocket);
             EndPoint dummyEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
-            var listener = new DiscoveryListener(fakeSocketFactory, fakeResponsFactory, fakeMessageParser, fakeDeviceInfoCollector);
-            A.CallTo(() => fakeSocket.EndReceiveFrom(A<IAsyncResult>.Ignored, ref dummyEndPoint)).
-                Invokes(() => fakeSocket.EndReceiveFrom(A<IAsyncResult>.Ignored, ref dummyEndPoint));
+            var listener = new DiscoveryListener(fakeSocketFactory, fakeResponsFactory, messageParser, fakeDeviceInfoCollector);
+
             listener.Listen();
+        }
+    }
+
+    internal class MockedAsyncResult : IAsyncResult
+    {
+        public bool IsCompleted { get; }
+        public WaitHandle AsyncWaitHandle { get; }
+        public object AsyncState { get; }
+        public bool CompletedSynchronously { get; }
+
+        public MockedAsyncResult(object state)
+        {
+            AsyncState = state;
+        }
+    }
+
+    internal class MockedSocket : ISocket
+    {
+        private const string messageString =
+            "M-SEARCH * HTTP/1.1\r\n" +
+            "LOCATION: http://1.1.1.1/service.xml\r\n" +
+            "\r\n";
+
+        public IAsyncResult BeginReceiveFrom(byte[] buffer, ref EndPoint remoteEndPoint, AsyncCallback asynCallback, object state)
+        {
+            var ar = new MockedAsyncResult(state);
+            Encoding.ASCII.GetBytes(messageString).CopyTo(buffer, 0);
+
+            asynCallback.Invoke(ar);
+
+            return ar;
+        }
+
+        public int EndReceiveFrom(IAsyncResult ar, ref EndPoint remoteEndPoint)
+        {
+            return messageString.Length;
+        }
+
+        public int ReceiveFrom(byte[] buffer, ref EndPoint remoteEndPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SendTo(byte[] buffer, EndPoint remoteEndPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
         }
     }
 }
